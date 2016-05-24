376425ebbfef440bf28d62988093a5b414184ce1
Bird <apw7177@gmail.com>
2016-05-24 16:10:56 -0400

    add AppComponent to set selectedHero and check if one has been selected or not in the template with *ngIf

--- app/app.component.ts
+++ app/app.component.ts
@@ -10,15 +10,17 @@ export class Hero {
     template:`
     <h1>{{title}}</h1>
     <ul class="heroes">
-      <li *ngFor="let hero of heroes">
+      <li *ngFor="let hero of heroes" (click)="onSelect(hero)">
         <span class="badge">{{hero.id}}</span> {{hero.name}}
       </li>
     </ul>
-    <h2>{{hero.name}} details!</h2>
-    <div><label>id: </label>{{hero.id}}</div>
-    <div>
-      <label>name: </label>
-      <input [(ngModel)]="hero.name" placeholder="name">
+    <div *ngIf="selectedHero">
+      <h2>{{selectedHero.name}} details!</h2>
+      <div><label>id: </label>{{selectedHero.id}}</div>
+      <div>
+        <label>name: </label>
+        <input [(ngModel)]="hero.name" placeholder="name">
+      </div>
     </div>
     `,
     styles:[`
@@ -73,11 +75,9 @@ export class Hero {
 })
 export class AppComponent {
   title = 'Tour of Heroes';
-  hero = Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
   public heroes = HEROES;
+  selectedHero: Hero;
+  onSelect(hero: Hero) { this.selectedHero = hero; }
 }
 
 var HEROES: Hero[] = [
