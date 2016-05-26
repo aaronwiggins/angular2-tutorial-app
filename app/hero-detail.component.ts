869e04664908cd7924f0c360a19c6f57c560954a
Bird <apw7177@gmail.com>
2016-05-26 12:14:29 -0400

    add the getHero method to the hero.sevice.

--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -1,6 +1,10 @@
-import { Component, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { Hero } from './hero';
+import { HeroService } from './hero.service';
+
+import { RouteParams } from '@angular/router-deprecated';
+
 
 @Component({
   selector: 'my-hero-detail',
@@ -15,7 +19,18 @@ import { Hero } from './hero';
     </div>
   `
 })
-export class HeroDetailComponent {
-  @Input()
-  hero: Hero;
+export class HeroDetailComponent implements OnInit {
+
+  constructor(
+    private heroService: HeroService,
+    private routeParams: RouteParams
+    ) {}
+
+    ngOnInit() {
+      let id = +this.routeParams.get('id');
+      this.heroService.getHero(id)
+        .then(hero => this.hero = hero);
+    }
+
+  @Input() hero: Hero;
 }
